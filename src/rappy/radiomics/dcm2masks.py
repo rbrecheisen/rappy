bd27a5040156057c243304cfbbb67637e1b8a375
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2018-03-02 16:34:03 +0100

    Saving work

--- src/rappy/radiomics/dcm2masks.py
+++ src/rappy/radiomics/dcm2masks.py
@@ -7,11 +7,15 @@ class Dcm2Masks(Node):
     value) in the DICOM file.
     """
     def __init__(self):
+
         super(Dcm2Masks, self).__init__()
+
         self.add_input('dcm_file', data_type='string')
         self.add_param('label_map', data_type='string')
-        self.add_output('output_file_paths', data_type='string')
+        self.add_output('output_files', data_type='string')
 
     def execute(self):
-        output_file_paths = []
-        self.set_output('output_file_paths', output_file_paths)
+
+        print('Dcm2Masks.execute()')
+        output_files = []
+        self.set_output('output_files', output_files)
